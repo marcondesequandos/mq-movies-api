f43085abb0fbc9f4ee823ff9a1f7707e342d8bca
marcondesequandos <felipemgcarvalho@gmail.com>
2024-02-01 11:19:07 -0300

    add comment about filters

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,8 @@ app.get("/mDiscover", async (req, res) => {
   try {
     const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&page=1&sort_by=popularity.desc`;
 
+    //Filtros para trabalhar:
+
     // includeAdult
     // includeVideo
     // page
