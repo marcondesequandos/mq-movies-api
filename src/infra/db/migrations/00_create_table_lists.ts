7166f9052707506c40936ef948bb805305ee2ede
marcondesequandos <felipemgcarvalho@gmail.com>
2024-03-05 07:05:03 -0300

    criando migrations

---
+++ src/infra/db/migrations/00_create_table_lists.ts
@@ -0,0 +1,29 @@
+import { UserModel } from "@/infra/modules/user/user.model";
+import { DataTypes, Sequelize } from "sequelize";
+import { MigrationFn } from "umzug";
+
+export const up: MigrationFn<Sequelize> = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().createTable("lists", {
+    id: {
+      type: DataTypes.STRING(255),
+      primaryKey: true,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      references: {
+        model: UserModel,
+        key: "id",
+      },
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+  });
+};
+
+export const down: MigrationFn<Sequelize> = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().dropTable("lists");
+};
