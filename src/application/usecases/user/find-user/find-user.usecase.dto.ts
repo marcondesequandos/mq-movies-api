737e77771b9ad36d8a22b970c89d5d0a1afae4d6
marcondesequandos <felipemgcarvalho@gmail.com>
2024-03-31 14:35:03 -0300

    finduser dto

--- src/application/usecases/user/find-user/find-user.usecase.dto.ts
+++ src/application/usecases/user/find-user/find-user.usecase.dto.ts
@@ -1,3 +1,30 @@
+import { ListType } from "@/application/entities/user/list";
+
 export interface FindUserUseCaseInputDTO {
   id: string;
+}
+
+export interface FindUserUseCaseOutputDto {
+  id: string;
+  name: string;
+  email: string;
+  lists: {
+    id: string;
+    name: string;
+    type: ListType;
+    items: {
+      adult: boolean;
+      backdrop_path: string;
+      id: string;
+      original_language: string;
+      original_title: string;
+      overview: string;
+      popularity: number;
+      poster_path: string;
+      release_date: string;
+      title: string;
+      vote_average: number;
+      vote_count: number;
+    }[];
+  }[];
 }
