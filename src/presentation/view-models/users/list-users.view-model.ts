283c456734d8f8a677c3f86d2484858af60184c3
marcondesequandos <felipemgcarvalho@gmail.com>
2024-05-07 09:36:53 -0300

    add listusers viewmodel

---
+++ src/presentation/view-models/users/list-users.view-model.ts
@@ -0,0 +1,29 @@
+import User from "@/application/entities/user/user";
+
+export class ListUsersViewModel {
+  id?: number;
+  name?: string;
+  email?: string;
+  lists?: {
+    id?: number;
+    name?: string;
+  }[];
+  created_at?: Date;
+  updated_at?: Date;
+
+  static map(users: User[]): ListUsersViewModel[] {
+    if (!users.length) return [];
+
+    return users.map((user) => ({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      lists: user.lists.map((list) => ({
+        id: list.id,
+        name: list.name,
+      })),
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    }));
+  }
+}
